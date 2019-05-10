c3ada72c470f3945e1577a535127a129b4c1fbda
csvernon <csvernon@ncsu.edu>
2019-05-10 07:55:14 -0400

    fake friends

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+const friends = [
+    {
+        name: 'Brad',
+        photo: "https://media.architecturaldigest.com/photos/5bbd0ab8d249622e01c312e9/16:9/w_1280,c_limit/GettyImages-1043027016.jpg",
+        ansArray: ['4','2','2','1','3','2','3','4','2','5']
+    },
+    {
+        name: 'Vin',
+        photo: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fnatalierobehmed%2Ffiles%2F2015%2F06%2Fdiesel-close.jpg",
+        ansArray: ['2','4','3','5','3','2','3','1','4','3']
+    },
+    {
+        name: 'Dave',
+        photo: "https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F838679%252Fe9aa5b93-2ff1-4f64-940f-3f01811b4894.jpg%252F950x534__filters%253Aquality%252890%2529.jpg?signature=EGBAdtLWrooQtMDmEm8Mfzgsv3c=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com",
+        ansArray: ['3','2','5','4','2','4','1','4','3','5']
+    },
+    {
+        name: 'Zoe',
+        photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2016611/rs_1024x696-160711102110-1024.Zoe-Saldana-7-Campaign.jpg?fit=inside|900:auto&output-quality=90",
+        ansArray: ['3','4','2','1','3','2','3','3','5','2']
+    },
+    {
+        name: 'Chris',
+        photo: "https://www.telegraph.co.uk/content/dam/films/2016/11/29/JS113926416chrispratt_trans_NvBQzQNjv4BqAkxErwRCehJfeaSerTYlkuQVrQw-c-qmpzxuMaWpIXU.jpg?imwidth=450",
+        ansArray: ['3','4','1','5','2','4','3','2','5','3']
+    },
+];
+
+module.exports = friends;
